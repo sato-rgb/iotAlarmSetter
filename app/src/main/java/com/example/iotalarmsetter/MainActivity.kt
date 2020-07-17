8387a0a374dae2e0c3791eaa749be7da108d3ba4
sato <satoyann0311@gmail.com>
2020-07-17 09:54:13 +0900

    add time left reload when onResume

--- app/src/main/java/com/example/iotalarmsetter/MainActivity.kt
+++ app/src/main/java/com/example/iotalarmsetter/MainActivity.kt
@@ -13,6 +13,7 @@ import android.view.View.VISIBLE
 import android.widget.DatePicker
 import android.widget.TimePicker
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatDelegate
 import com.github.kittinunf.fuel.Fuel
 import kotlinx.android.synthetic.main.activity_main.*
 import com.github.kittinunf.fuel.httpGet
@@ -32,15 +33,18 @@ class MainActivity : AppCompatActivity() {
         var hour: Int,
         var minute: Int
     )
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //theme_related()
+        Log.d("NightMode","${AppCompatDelegate.getDefaultNightMode()}")
         setContentView(R.layout.activity_main)
 
         simpleTimePicker.setIs24HourView(true)
 
 
-        //設定の読み込み（古）
+
+            //設定の読み込み（古）
 
         /*var alarm_setting = getSharedPreferences("Alarm", MODE_PRIVATE)
         simpleTimePicker.hour = alarm_setting.getInt("hour",7)
@@ -57,10 +61,10 @@ class MainActivity : AppCompatActivity() {
                 when (result) {
                     is Result.Success -> {
                         // レスポンスボディを表示
-                        debug_text.setText("停止")
+                        debug_text.text = "停止"
                     }
                     is Result.Failure -> {
-                        debug_text.setText("停止に失敗しました。")
+                        debug_text.text = "停止に失敗しました。"
                     }
                 }
 
@@ -68,7 +72,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         send_setting.setOnClickListener{
-            debug_text.setText("button pushed!!")
+            debug_text.text = "button pushed!!"
             val alarm = toggle_alart.isChecked
             val hour = simpleTimePicker.hour
             val minute = simpleTimePicker.minute
@@ -81,20 +85,25 @@ class MainActivity : AppCompatActivity() {
                         when (result) {
                             is Result.Success -> {
                                 // レスポンスボディを表示
-                                debug_text.setText("設定を送信しました。")
+                                debug_text.text = "設定を送信しました。"
                                 send_setting.visibility = View.INVISIBLE
                                 send_stop.visibility = VISIBLE
                             }
                             is Result.Failure -> {
-                                debug_text.setText("通信に失敗しました。")
+                                debug_text.text = "通信に失敗しました。"
                             }
                         }
                     }
-            time_left_reload()
+
             }
 
 
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+        time_left_reload()
     }
 
     //アラームが鳴るまでの時間を表示する
@@ -115,7 +124,7 @@ class MainActivity : AppCompatActivity() {
         hour_left += hour_target - hour_now
         if (hour_left < 0){hour_left += 24}
 
-        alarm_minutes_left.setText("${hour_left}:${minute_left} time left")
+        alarm_minutes_left.text = "${hour_left}:${minute_left} time left"
     }
     fun setting_save (b: Boolean,h:Int,m:Int){
         var alarm_setting = getSharedPreferences("Alarm", MODE_PRIVATE)
@@ -128,10 +137,10 @@ class MainActivity : AppCompatActivity() {
     }
     fun setting_load(){
         //Async
-        "http://192.168.0.141:10458/alarm/get".httpGet().responseString(){ _, _, result ->
+        "http://192.168.0.141:10458/alarm/get".httpGet().responseString { _, _, result ->
             when(result){
                 is Result.Failure ->{
-                    debug_text.setText("設定の読み込みに失敗しました。")
+                    debug_text.text = "設定の読み込みに失敗しました。"
                 }
                 is Result.Success ->{
                     val gson = Gson()
@@ -139,15 +148,16 @@ class MainActivity : AppCompatActivity() {
 
                     simpleTimePicker.hour = data.hour
                     simpleTimePicker.minute = data.minute
-                    toggle_alart.setChecked(data.alarm)
-                    debug_text.setText("設定の読み込みに成功しました。")
+                    toggle_alart.isChecked = data.alarm
+                    debug_text.text = "設定の読み込みに成功しました。"
                 }
-            }        //設定を変更したら送信ボタンが出現
+            }        //設定を変更したら送信ボタンが出現し鳴るまでの時間が更新される
             //ここにあるべきではない
             //並列で読み込まれるせいであるべきところに置くとバグります
             simpleTimePicker.setOnTimeChangedListener { _, _,_ ->
                 send_setting.visibility = VISIBLE
                 send_stop.visibility = View.INVISIBLE
+                time_left_reload()
             }
             toggle_alart.setOnClickListener {
                 send_setting.visibility = VISIBLE
@@ -156,4 +166,32 @@ class MainActivity : AppCompatActivity() {
             time_left_reload()
         }}
 
-}
+    fun theme_related(){
+        val theme = getSharedPreferences("dark", MODE_PRIVATE)
+        val editor = theme.edit()
+        val j = theme.getBoolean("need_dark",false)
+        when (j) {
+            true -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                editor.putBoolean("need_dark",true)
+            }
+            else -> {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                editor.putBoolean("need_dark",false)
+            }}
+        toggle_dark.setOnClickListener{
+            val i = theme.getBoolean("need_dark",false)
+            when (i) {
+                true -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    editor.putBoolean("need_dark",true)
+                }
+                else -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    editor.putBoolean("need_dark",false)
+                }
+            }
+        }}
+    }
+
+
